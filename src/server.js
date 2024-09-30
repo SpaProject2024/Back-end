9b73a80574cc42a18698ce7cf0592d18c4e476b4
Huynh Pham Tam Nguyen <tamnguyen.lisa@gmail.com>
2024-09-30 08:37:18 +0700

    Update Project

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 import express from "express";
 import connectDB from "./config/db.js";
-import testRoutes from "./routes/testRouters.js";
-import doctorsRoutes from "./routes/doctorsRouter.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import route from"./routes/index.js"
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+app.use(morgan("combined"));
 app.use(express.json());
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/tests", testRoutes);
-app.use("/doctors", doctorsRoutes);
+route(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
